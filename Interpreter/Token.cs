fdaf0f862690934b1116d3b6e161518e90b93514
AldrenKim <69776513+AldrenKim@users.noreply.github.com>
2021-02-17 13:07:20 +0800

    Defined Token class

--- Interpreter/Token.cs
+++ Interpreter/Token.cs
@@ -8,5 +8,50 @@ namespace Interpreter
 {
     class Token
     {
+        private TokenType type;
+        private String lexeme;
+        private Object literal;
+        private int line;
+
+        public Token(TokenType type, String lexeme, Object literal, int line)
+        {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+
+        public TokenType Type{
+            get
+            {
+                return type;
+            }
+
+        }
+
+        public String Lexeme
+        {
+            get
+            {
+                return lexeme;
+            }
+        }
+
+        public Object Literal
+        {
+            get
+            {
+                return literal;
+            }
+        }
+        
+        public int Line
+        {
+            get
+            {
+                return line;
+            }
+        }
+
     }
 }
--- Interpreter/TokenType.cs
+++ Interpreter/TokenType.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Interpreter
 {
-    class TokenType
+    public enum TokenType
     {
+        LEFT_PAREN, RIGHT_PAREN, //( )
+        LEFT_BRACE, RIGHT_BRACE, //[]   
+
+        COMMA, EQUALS, COLON,  // , . * :
+        SHARP, AMPERSAND, //# &
+        MULT, ADD, SUBT, DIV, MOD,
+
+        GREATER, LESSER, // > <
+        GREATER_EQUAL, LESSER_EQUAL, // >= <=
+
+        EQUAL, NOT_EQUAL, // == <>
+
+
+        IDENTIFIER, //^([A-Za-z+_+$][A-Za-z+_+$]*)
+        CHAR_LIT, //^('.*')
+        INT_LIT,  //^((-)[0-9]{1,32})
+        FLOAT_LIT,  //^() ambot unsay regex ani
+        BOOL_LIT,  //^(TRUE|FALSE)
+
+            //RESERVED WORDS
+        VAR, AS, OUTPUT, IF, ELSE, WHILE, START, STOP, INT, BOOL, FLOAT, CHAR, EOF,
+        AND, OR, NOT
     }
 }
